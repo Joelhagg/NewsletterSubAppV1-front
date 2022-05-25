b2a3df2729a0332c127686034c262d24a7ef056f
joelhagg <joel.pettersson90@gmail.com>
2022-05-25 16:15:34 +0200

    add styling and frontend functions for login handling

--- login/login.js
+++ login/login.js
@@ -1,13 +1,40 @@
 console.log("Login test");
 
-let user = {};
-
 document.getElementById("loginButton").addEventListener("click", (e) => {
   e.preventDefault();
   console.log("click");
 
   let userEmail = document.getElementById("userEmail").value;
   let userPassword = document.getElementById("userPassword").value;
+  let loginUserInformation = {};
 
   console.log(userEmail, userPassword);
+
+  let user = {
+    email: userEmail,
+    password: userPassword,
+  };
+
+  const postLogin = async () => {
+    try {
+      await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      })
+        .then((response) => response.json())
+        .then((result) => (loginUserInformation = result));
+    } catch (error) {
+      console.log("error ", error);
+    }
+    localStorage.setItem(
+      "loginUserInformation",
+      JSON.stringify(loginUserInformation)
+    );
+    window.location.href = "../index.html";
+  };
+
+  postLogin();
 });
--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 console.log("test");
+
+let isAuth = false;
+let loggedInConatiner = document.getElementById("loggedInConatiner");
+let registerLoginContainer = document.getElementById("registerLoginContainer");
+
+const initApp = () => {
+  let localStorageUserString = localStorage.getItem("loginUserInformation");
+  let localStorageUserParsed = JSON.parse(localStorageUserString);
+  console.log("localStorageUser", localStorageUserParsed);
+
+  if (localStorageUserParsed) {
+    isAuth = true;
+  }
+
+  if (isAuth) {
+    loggedInConatiner.classList.remove("isAuth");
+    registerLoginContainer.classList.add("isAuth");
+  } else {
+    loggedInConatiner.classList.add("isAuth");
+    registerLoginContainer.classList.remove("isAuth");
+  }
+
+  document.getElementById("logOutButton").addEventListener("click", () => {
+    console.log("click");
+    localStorage.clear();
+    isAuth = false;
+    location.reload();
+  });
+};
+
+initApp();
